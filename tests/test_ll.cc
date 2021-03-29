1d18fcd9d6434e8dc885c678d036644d62cade8e
angeldong0826 <adong6@illinois.edu>
2021-03-29 02:08:19 -0500

    Added mixture test cases.

--- tests/test_ll.cc
+++ tests/test_ll.cc
@@ -19,13 +19,24 @@ TEST_CASE("Push Back", "[constructor][push_back][size][empty]") {
 
   SECTION("Push back one element") {
     list.push_back(1);
+
     REQUIRE(list.size() == 1);
   }
 
-  SECTION("Push back two elements") {
+  SECTION("Push back multiple elements") {
     list.push_back(-1);
     list.push_back(10000);
+
     REQUIRE(list.size() == 2);
+  }
+
+  SECTION("Double Push Back Order Check") {
+    list.push_back(-1);
+    list.push_back(10000);
+
+    REQUIRE(list.size() == 2);
+    REQUIRE(list.front() == -1);
+    REQUIRE(list.back() == 10000);
   }
 }
 
@@ -37,14 +48,52 @@ TEST_CASE("Push Front") {
 
   SECTION("Push front one element") {
     list.push_front(-1);
+
     REQUIRE(list.size() == 1);
   }
 
-  SECTION("Push front two elements") {
+  SECTION("Push front multiple elements") {
     list.push_front(-1);
     list.push_front(10000);
+
     REQUIRE(list.size() == 2);
   }
+
+  SECTION("Double Push Back Order Check") {
+    list.push_front(-1);
+    list.push_front(10000);
+
+    REQUIRE(list.size() == 2);
+    REQUIRE(list.back() == -1);
+    REQUIRE(list.front() == 10000);
+  }
+}
+
+TEST_CASE("Push Front and Back") {
+  LinkedList<int> list;
+
+  REQUIRE(list.size() == 0);
+  REQUIRE(list.empty());
+
+  SECTION("Minimum Two elements") {
+    list.push_front(-1);
+    list.push_back(10000);
+
+    REQUIRE(list.size() == 2);
+    REQUIRE(list.front() == -1);
+    REQUIRE(list.back() == 10000);
+  }
+
+  SECTION("Multiple Elements") {
+    list.push_front(-1);
+    list.push_back(10000);
+    list.push_back(32);
+
+    REQUIRE(list.size() == 3);
+    REQUIRE(list.front() == -1);
+    REQUIRE(list.back() == 32);
+  }
+
 }
 
 TEST_CASE("Size") {
@@ -56,18 +105,22 @@ TEST_CASE("Size") {
 
   SECTION("One element in list") {
     list.push_front(10000);
+
     REQUIRE(list.size() == 1);
   }
 
   SECTION("Multiple elements in list") {
     list.push_front(-1);
     list.push_front(10000);
-    REQUIRE(list.size() == 2);
+    list.push_front(100);
+
+    REQUIRE(list.size() == 3);
   }
 }
 
 TEST_CASE("Front") {
   LinkedList<int> list;
+
   REQUIRE(list.size() == 0);
   REQUIRE(list.empty());
 
@@ -76,12 +129,14 @@ TEST_CASE("Front") {
 
   SECTION("One element in list") {
     list.push_front(10000);
+
     REQUIRE(list.front() == 10000);
   }
 
   SECTION("Multiple elements in list") {
     list.push_front(-1);
     list.push_front(10000);
+
     REQUIRE(list.front() == 10000);
   }
 }
@@ -97,13 +152,42 @@ TEST_CASE("Back") {
 
   SECTION("One element in list") {
     list.push_front(10000);
+
     REQUIRE(list.back() == 10000);
   }
 
   SECTION("Multiple elements in list") {
     list.push_front(-1);
     list.push_front(10000);
+
     REQUIRE(list.back() == -1);
+  }
+}
+
+TEST_CASE("Call on Front and Back") {
+  LinkedList<int> list;
+
+  REQUIRE(list.size() == 0);
+  REQUIRE(list.empty());
+
+  SECTION("Front and Back Points to Same Element when Only One Exists") {
+    list.push_back(10);
+
+    REQUIRE(list.front() == 10);
+    REQUIRE(list.back() == 10);
+    REQUIRE(list.front() == list.back());
+  }
+
+  SECTION("Check front and back together") {
+    list.push_front(-1);
+    list.push_front(10000);
+    list.push_back(89);
+    list.push_back(689);
+
+
+    REQUIRE(list.size() == 4);
+    REQUIRE(list.front() == 10000);
+    REQUIRE(list.back() == 689);
   }
 }
 
@@ -119,12 +203,12 @@ TEST_CASE("Clear") {
   SECTION("One element in list") {
     list.push_front(10);
     list.clear();
+
     REQUIRE(list.size() == 0);
     REQUIRE(list.empty());
   }
 
   SECTION("Multiple elements in list") {
-
   }
 }
 
@@ -140,6 +224,7 @@ TEST_CASE("Pop Front") {
   SECTION("One element in list") {
     list.push_front(10);
     list.pop_front();
+
     REQUIRE(list.size() == 0);
   }
 
@@ -147,8 +232,18 @@ TEST_CASE("Pop Front") {
     list.push_front(10);
     list.push_back(10000);
     list.pop_front();
+
     REQUIRE(list.size() == 1);
     REQUIRE(list.front() ==  10000);
+  }
+
+  SECTION("Multiple pop front") {
+    list.push_front(10);
+    list.push_back(10000);
+    list.pop_front();
+    list.pop_front();
+
+    REQUIRE(list.size() == 0);
   }
 }
 
@@ -158,12 +253,12 @@ TEST_CASE("Pop Back") {
   REQUIRE(list.size() == 0);
   REQUIRE(list.empty());
 
-  SECTION("No element in list") {
-  }
+  SECTION("No element in list") {}
 
   SECTION("One element in list") {
     list.push_back(10);
     list.pop_back();
+
     REQUIRE(list.size() == 0);
   }
 
@@ -171,6 +266,48 @@ TEST_CASE("Pop Back") {
     list.push_front(10);
     list.push_back(10000);
     list.pop_back();
+
     REQUIRE(list.size() == 1);
+    REQUIRE(list.front() == 10);
+    REQUIRE(list.back() == 10);
+  }
+
+  SECTION("Multiple pop back") {
+    list.push_front(10);
+    list.push_back(10000);
+    list.pop_back();
+
+    REQUIRE(list.front() == 10);
+    list.pop_back();
+
+    REQUIRE(list.size() == 0);
+  }
+}
+
+TEST_CASE("Pop Front and Back") {
+  LinkedList<int> list;
+
+  REQUIRE(list.size() == 0);
+  REQUIRE(list.empty());
+
+  SECTION("Minimum Two elements") {
+    list.push_front(-1);
+    list.push_back(10000);
+    list.pop_back();
+    list.pop_front();
+
+    REQUIRE(list.size() == 0);
+  }
+
+  SECTION("Multiple Elements") {
+    list.push_front(-1);
+    list.push_back(10000);
+    list.push_back(32);
+    list.pop_front();
+    list.pop_back();
+
+    REQUIRE(list.size() == 1);
+    REQUIRE(list.front() == 10000);
+    REQUIRE(list.back() == 10000);
   }
 }
